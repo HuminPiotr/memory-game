c7083402880e777ec0d4524c2a20e376fee6abc9
Piotr <humin.piotr@gmail.com>
2021-07-07 12:25:58 +0200

    separation BackgroundNoise component

--- src/views/EndView/EndView.jsx
+++ src/views/EndView/EndView.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { endGame } from '../../actions';
 
-
+import BackgroundNoise from '../../components/BackgroundNoise/BackgroundNoise';
 import ScoreList from '../../components/ScoreList/ScoreList.jsx';
 
 
@@ -16,37 +16,7 @@ const Table = styled.div`
     border: solid teal 2px;
 
 `
-const Background = styled.div`
-    position: fixed;
-    top: -50%;
-    left: -50%;
-    right: -50%;
-    bottom: -50%;
-    width: 200%;
-    height: 200vh;
-    background: transparent url('http://assets.iceable.com/img/noise-transparent.png') repeat 0 0;
-    background-repeat: repeat;
-    animation: bg-animation .2s infinite;
-    opacity: .9;
-    visibility: visible;
-    z-index: -1;
 
-    @keyframes bg-animation {
-    0% { transform: translate(0,0) }
-    10% { transform: translate(-5%,-5%) }
-    20% { transform: translate(-10%,5%) }
-    30% { transform: translate(5%,-10%) }
-    40% { transform: translate(-5%,15%) }
-    50% { transform: translate(-10%,5%) }
-    60% { transform: translate(15%,0) }
-    70% { transform: translate(0,10%) }
-    80% { transform: translate(-15%,0) }
-    90% { transform: translate(10%,5%) }
-    100% { transform: translate(5%,0) }
-}
-
-
-`
 
 const StyledWrapper = styled.div`
     display: flex;
@@ -70,7 +40,7 @@ const StyledWrapper = styled.div`
 
 const EndView = ({ endGame }) => (
     <>
-        <Background /> 
+        <BackgroundNoise /> 
         <StyledWrapper>
             <h1>Best of the best</h1>
             <Table>
--- src/views/StartView/StartView.jsx
+++ src/views/StartView/StartView.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header/Header'
+import BackgroundNoise from '../../components/BackgroundNoise/BackgroundNoise';
 
 const StyledWrapper = styled.div`
     height: calc(100vh - 100px);
@@ -55,36 +56,6 @@ const StyledWrapper = styled.div`
         border-radius: 20px;
     }
 `
-const Background = styled.div`
-    position: fixed;
-    top: -50%;
-    left: -50%;
-    right: -50%;
-    bottom: -50%;
-    width: 200%;
-    height: 200vh;
-    background: transparent url('http://assets.iceable.com/img/noise-transparent.png') repeat 0 0;
-    background-repeat: repeat;
-    animation: bg-animation .2s infinite;
-    opacity: .9;
-    visibility: visible;
-    z-index: -1;
-
-    @keyframes bg-animation {
-    0% { transform: translate(0,0) }
-    10% { transform: translate(-5%,-5%) }
-    20% { transform: translate(-10%,5%) }
-    30% { transform: translate(5%,-10%) }
-    40% { transform: translate(-5%,15%) }
-    50% { transform: translate(-10%,5%) }
-    60% { transform: translate(15%,0) }
-    70% { transform: translate(0,10%) }
-    80% { transform: translate(-15%,0) }
-    90% { transform: translate(10%,5%) }
-    100% { transform: translate(5%,0) }
-}
-`
-
 
 const StartView = () => {
     localStorage.setItem('PLAYER_NAME', 'Anonim');
@@ -95,15 +66,13 @@ const StartView = () => {
 
     return(
     <>
-    <Background>
-    </Background>
+    <BackgroundNoise />
     <StyledWrapper>
-        {/* <h1>Memory</h1> */}
         <Header>Memory</Header>
         <div className="inputWrapper">
             <label htmlFor="name">Player name</label> <br></br>
             <input type="text"  name="name" id="name" maxLength = '20' onChange={handleChange}/>
-            <Link to="/game" className="button" >Play</Link>
+            <Link to="/game" className="button">Play</Link>
         </div>
         
     </StyledWrapper>
