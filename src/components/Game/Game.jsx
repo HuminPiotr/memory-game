4f547687671c34bea2b2ea8ae0581ff3ac50a9a0
Piotr <humin.piotr@gmail.com>
2021-07-07 12:02:17 +0200

    change images

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -12,12 +12,12 @@ import Timer from '../Timer/Timer.jsx';
 import TotalFlips from '../TotalFlips/TotalFlips.jsx';
 
 //images
-import alien from '../../assets/images/alien.svg';
-import dog from '../../assets/images/dog.svg';
-import monkey from '../../assets/images/monkey.svg';
-import panda from '../../assets/images/panda.svg';
-import pig from '../../assets/images/pig.svg';
-import wolf from '../../assets/images/wolf.svg';
+import image1 from '../../assets/images/1.svg';
+import image2 from '../../assets/images/2.svg';
+import image3 from '../../assets/images/3.svg';
+import image4 from '../../assets/images/4.svg';
+import image5 from '../../assets/images/5.svg';
+import image6 from '../../assets/images/6.svg';
 
 const GameWrapper = styled.div`
     padding: 25px;
@@ -62,7 +62,7 @@ const Game = ({ startGame, pairsFound, gameComplete, completeGame}) => {
          
         const playerName = localStorage.getItem('PLAYER_NAME');
         const pictureList = [
-            alien, alien, dog, dog, monkey, monkey, panda, panda, wolf, wolf, pig, pig
+            image1, image1, image2, image2, image3, image3, image4, image4, image5, image5, image6, image6
         ]        //od liczby par zależeć będzie ile kart jest wygenerowanych
 
 
